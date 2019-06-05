1a0286851ac99bc1b5ad1653850351ff5d8db1eb
einiemand-sl <einiemand@outlook.com>
2019-06-05 12:05:53 -0700

    Test code in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include "Error.hpp"
+#include "Tokenizer.hpp"
+#include <sstream>
 using namespace UrSQL;
 
 int main(int argc, char* argv[])
 {
+	std::string userInput;
+	while (true) {
+		defaultOutput << "> ";
+		while (std::getline(std::cin, userInput, ';')) {
+			std::istringstream inputStream(userInput);
+			Tokenizer theTokenizer(inputStream);
+			theTokenizer.tokenize();
+			Token theToken = theTokenizer.current();
+			std::cout << theToken.get_data() << '\n';
+		}
+	}
 
 	return 0;
 }
