98cc5df94f6ef61aa888c9f7998bd720eebafa6b
einiemand-sl <einiemand@outlook.com>
2019-06-10 00:42:25 -0700

    Update Storable: each Storable object stores its block number - where it is stored in file

--- src/Storable.hpp
+++ src/Storable.hpp
@@ -10,17 +10,23 @@ using blocknum_t = int32_t;
 
 class Storable {
 public:
-	Storable() = default;
+	Storable(blocknum_t aBlockNum = -1) : m_blocknum(aBlockNum)
+	{
+	}
 
 	Storable(const Storable&) = delete;
 	Storable& operator=(const Storable&) = delete;
 
 	virtual void encode(Block& aBlock) const = 0;
-	virtual void decode(const Block& aBlock) = 0;
+	virtual void decode(const Block& aBlock, blocknum_t aBlockNum) = 0;
 	virtual ~Storable() = 0 {}
 
 	inline blocknum_t get_blocknum() const {
 		return m_blocknum;
+	}
+
+	inline void set_blocknum(blocknum_t aBlockNum) {
+		m_blocknum = aBlockNum;
 	}
 
 protected:
