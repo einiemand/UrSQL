9f69f8edfb87653daed73c541f038016bc8a741c
einiemand-sl <einiemand@outlook.com>
2019-06-10 00:49:20 -0700

    Add abstrct class Interpreter to process input and execute Statements

---
+++ src/Interpreter.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef INTERPRETER_HPP
+#define INTERPRETER_HPP
+#include "Error.hpp"
+#include "Statement.hpp"
+#include <memory>
+
+namespace UrSQL {
+
+class Tokenizer;
+class Database;
+
+class Interpreter {
+public:
+	Interpreter(Interpreter* anInterpreter) :
+		m_next(anInterpreter)
+	{
+	}
+
+	Interpreter(const Interpreter&) = delete;
+	Interpreter& operator=(const Interpreter&) = delete;
+
+	virtual std::unique_ptr<Statement> get_statement(Tokenizer& aTokenizer) const = 0;
+	virtual ~Interpreter() = 0 {}
+
+	StatusResult process_input(Tokenizer& aTokenizer) {
+		if (auto theStatement = get_statement(aTokenizer)) {
+			StatusResult theResult = theStatement->parse();
+			if (theResult) {
+				theResult = theStatement->validate();
+				if (theResult) {
+					theResult = theStatement->run();
+				}
+			}
+			return theResult;
+		}
+
+		return m_next ? m_next->process_input(aTokenizer) :
+			StatusResult(Error::unknown_command, "A valid command should start with a keyword");
+	}
+
+	virtual Database* get_active_database() const {
+		return m_next ? m_next->get_active_database() : nullptr;
+	}
+protected:
+	Interpreter* m_next;
+};
+
+}
+
+#endif
