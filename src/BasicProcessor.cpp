e1d7201c1438e4a5827bc1deb62e1f4e15be116f
einiemand-sl <einiemand@outlook.com>
2019-06-10 00:50:03 -0700

    Add BasicProcessor: it can handle 'help', 'version', 'quit'

---
+++ src/BasicProcessor.cpp
@@ -0,0 +1,24 @@
+#include "BasicProcessor.hpp"
+#include "Statement.hpp"
+#include "Tokenizer.hpp"
+
+namespace UrSQL {
+
+BasicProcessor::BasicProcessor(Interpreter* anInterpreter) :
+	Interpreter(anInterpreter)
+{
+}
+
+std::unique_ptr<Statement> BasicProcessor::get_statement(Tokenizer& aTokenizer) const {
+	Keyword theKeyword = aTokenizer.peek().get_keyword();
+	switch (theKeyword) {
+	case Keyword::help_kw:
+	case Keyword::version_kw:
+	case Keyword::quit_kw:
+		return std::make_unique<BasicStatement>(aTokenizer, theKeyword);
+	default:
+		return nullptr;
+	}
+}
+
+}
---
+++ src/BasicProcessor.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef BASICPROCESSOR_HPP
+#define BASICPROCESSOR_HPP
+#include "Interpreter.hpp"
+
+namespace UrSQL {
+
+class BasicProcessor : public Interpreter {
+public:
+	BasicProcessor(Interpreter* anInterpreter = nullptr);
+	~BasicProcessor() = default;
+
+	std::unique_ptr<Statement> get_statement(Tokenizer& aTokenizer) const override;
+
+};
+
+}
+
+#endif
