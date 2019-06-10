9bb7d4ef991590eea4f2113fff130de236ed6a95
einiemand-sl <einiemand@outlook.com>
2019-06-10 00:50:58 -0700

    Add abstract class Statement and BasicStatement('help', 'version', 'quit')

---
+++ src/Statement.cpp
@@ -0,0 +1,45 @@
+#include "Statement.hpp"
+#include "Tokenizer.hpp"
+
+namespace UrSQL {
+
+BasicStatement::BasicStatement(Tokenizer& aTokenizer, Keyword aKeyword) :
+	Statement(aTokenizer),
+	m_keyword(aKeyword)
+{
+}
+
+StatusResult BasicStatement::parse() {
+	return StatusResult(Error::no_error);
+}
+
+StatusResult BasicStatement::validate() const {
+	return m_tokenizer.size() <= 1 ? StatusResult(Error::no_error) :
+		StatusResult(Error::invalid_command, "Redundant input after '" + m_tokenizer.peek().get_data() + '\'');
+}
+
+StatusResult BasicStatement::run() const {
+	switch (m_keyword) {
+	case Keyword::help_kw: {
+		static const char* theHelpMsg = 
+			"help    - show guide on how to use UrSQL.\n"
+			"version - show UrSQL version.\n"
+			"quit    - quit UrSQL.";
+		show_message(theHelpMsg);
+		break;
+	}
+	case Keyword::version_kw:
+		show_message("UrSQL 1.0");
+		break;
+	case Keyword::quit_kw:
+		show_message("Bye.");
+		return StatusResult(Error::user_terminated);
+	case Keyword::unknown_kw:
+		break;
+	default:
+		throw std::runtime_error("Impossible: A BasicStatement shouldn't have been run");
+	}
+	return StatusResult(Error::no_error);
+}
+
+}
---
+++ src/Statement.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef STATEMENT_HPP
+#define STATEMENT_HPP
+#include "Error.hpp"
+#include "Keyword.hpp"
+
+namespace UrSQL {
+
+class Tokenizer;
+
+class Statement {
+public:
+	Statement(Tokenizer& aTokenizer) : m_tokenizer(aTokenizer)
+	{
+	}
+
+	Statement(const Statement&) = delete;
+	Statement& operator=(const Statement&) = delete;
+
+	virtual StatusResult parse() = 0;
+	virtual StatusResult validate() const = 0;
+	virtual StatusResult run() const = 0;
+	virtual ~Statement() = 0 {}
+protected:
+	Tokenizer& m_tokenizer;
+};
+
+class BasicStatement : public Statement {
+public:
+	BasicStatement(Tokenizer& aTokenizer, Keyword aKeyword);
+	~BasicStatement() = default;
+
+	StatusResult parse() override;
+	StatusResult validate() const override;
+	StatusResult run() const override;
+private:
+	Keyword m_keyword;
+};
+
+}
+
+#endif /* Statement.hpp */
