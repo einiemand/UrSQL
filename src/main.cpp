9da341935f86f08bceb9f23e7cbe820b6288df2b
einiemand-sl <einiemand@outlook.com>
2019-06-10 00:51:24 -0700

    Update main() logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,65 @@
 #include "Error.hpp"
 #include "Tokenizer.hpp"
+#include "BasicProcessor.hpp"
 #include <sstream>
-using namespace UrSQL;
+#include <algorithm>
+
+void trim(std::string& aString) {
+	aString.erase(aString.begin(), std::find_if_not(aString.begin(), aString.end(), std::isspace));
+	aString.erase(aString.length() - (std::find_if_not(aString.rbegin(), aString.rend(), std::isspace) - aString.rbegin()));
+}
 
 int main(int argc, char* argv[])
 {
-	std::string userInput;
-	while (true) {
-		defaultOutput << "> ";
-		while (std::getline(std::cin, userInput, ';')) {
-			std::istringstream inputStream(userInput);
-			Tokenizer theTokenizer(inputStream);
-			theTokenizer.tokenize();
-			Token theToken = theTokenizer.current();
-			std::cout << theToken.get_data() << '\n';
+	using namespace UrSQL;
+	static const char* const thePrevMark = "\n> ";
+	BasicProcessor theBasicProcessor;
+
+	StatusResult theResult(Error::no_error);
+	while (theResult.get_code() != Error::user_terminated) {
+		defaultOutput << thePrevMark;
+		std::string theUserInput;
+		if (std::getline(std::cin, theUserInput, ';')) {
+			trim(theUserInput);
+			if (!theUserInput.empty()) {
+				std::istringstream theInputStream(theUserInput);
+				Tokenizer theTokenizer(theInputStream);
+				theResult = theTokenizer.tokenize();
+				if (theResult) {
+					theResult = theBasicProcessor.process_input(theTokenizer);
+				}
+				if (!theResult && theResult.get_code() != Error::user_terminated) {
+					theResult.show_error();
+				}
+			}
 		}
 	}
+	/*char theChar;
+
+	while (theResult.get_code() != Error::user_terminated) {
+		defaultOutput << thePrevMark;
+		std::string theUserInput;
+		while (std::cin.get(theChar) && theChar != ';') {
+			theUserInput += theChar;
+			if (theChar == '\n') {
+				defaultOutput << thePrevMark;
+			}
+		}
+		std::cin.ignore();
+
+		trim(theUserInput);
+		if (!theUserInput.empty()) {
+			std::istringstream theInputStream(theUserInput);
+			Tokenizer theTokenizer(theInputStream);
+			theResult = theTokenizer.tokenize();
+			if (theResult) {
+				theResult = theBasicProcessor.process_input(theTokenizer);
+			}
+			if (!theResult && theResult.get_code() != Error::user_terminated) {
+				theResult.show_error();
+			}
+		}
+	}*/
 
 	return 0;
 }
