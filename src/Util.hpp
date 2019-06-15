cd6d6ecd5139a24541246650c3183aac8088d632
einiemand-sl <einiemand@outlook.com>
2019-06-15 05:33:09 -0700

    Differentiate blocknum_t based on windows platforms and add StringList declaration

--- src/Util.hpp
+++ src/Util.hpp
@@ -3,11 +3,20 @@
 #define UTIL_HPP
 #include <xutility>
 #include <stdint.h>
+#include <vector>
+#include <string>
 
 namespace UrSQL {
 
 using size_type = std::size_t;
+
+#if _WIN64
 using blocknum_t = int32_t;
+#elif _WIN32
+using blocknum_t = int64_t;
+#endif
+
+using StringList = std::vector<std::string>;
 
 }
 
