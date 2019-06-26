f3d69a851d4a4e8f0d1d7ac07d25b00849be133d
einiemand-sl <einiemand@outlook.com>
2019-06-25 20:33:10 -0500

    Finish getStatement() and createTable()

--- src/SQLInterpreter.cpp
+++ src/SQLInterpreter.cpp
@@ -1,7 +1,38 @@
 #include "SQLInterpreter.hpp"
+#include "Tokenizer.hpp"
+#include "SQLStatement.hpp"
+#include "Database.hpp"
 
 namespace UrSQL {
 
+SQLInterpreter::SQLInterpreter(Interpreter* next) :
+	Interpreter(next)
+{
+}
 
+std::unique_ptr<Statement> SQLInterpreter::getStatement(Tokenizer& aTokenizer) {
+	Keyword theKeyword = aTokenizer.peek().getKeyword();
+
+	switch (theKeyword) {
+	case Keyword::create_kw: {
+		if (aTokenizer.remaining() > 1 && aTokenizer.peek(1).getKeyword() == Keyword::table_kw) {
+			return SQLStatement::factory(aTokenizer, *this);
+		}
+		break;
+	}
+	}
+	return nullptr;
+}
+
+StatusResult SQLInterpreter::createTable(const AttributeList& anAttributeList, const std::string& anEntityName) {
+	StatusResult theResult(Error::no_error);
+	if (Database* theActiveDB = getActiveDatabase()) {
+		theResult = theActiveDB->createTable(anAttributeList, anEntityName);
+	}
+	else {
+		theResult.setError(Error::unknown_database, "Specify the database first by 'use <DBName>'");
+	}
+	return theResult;
+}
 
 }
--- src/SQLInterpreter.hpp
+++ src/SQLInterpreter.hpp
@@ -2,10 +2,19 @@
 #ifndef SQLINTERPRETER_HPP
 #define SQLINTERPRETER_HPP
 #include "Interpreter.hpp"
+#include "Attribute.hpp"
 
 namespace UrSQL {
 
 class SQLInterpreter : public Interpreter {
+public:
+	SQLInterpreter(Interpreter* next);
+	~SQLInterpreter() override = default;
+
+	std::unique_ptr<Statement> getStatement(Tokenizer& aTokenizer) override;
+
+	StatusResult createTable(const AttributeList& anAttributeList, const std::string& anEntityName);
+private:
 
 };
 
