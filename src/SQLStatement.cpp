7c962390a5777a32c46ba9cd6f95196972481d0c
einiemand-sl <einiemand@outlook.com>
2019-06-29 22:20:38 -0500

    Update _parseTableName(): check if there are more tokens first. Add two helper functions to parse a sequence divided by comma and check if there are two or more same elements in a container. Add InsertIntoTableStatement: TODO: execute()

--- src/SQLStatement.cpp
+++ src/SQLStatement.cpp
@@ -16,16 +16,54 @@ SQLStatement::SQLStatement(Tokenizer& aTokenizer, SQLInterpreter& anInterpreter)
 
 StatusResult SQLStatement::_parseTableName() {
 	StatusResult theResult(Error::no_error);
-	const Token& theNameToken = m_tokenizer.get();
-	if (theNameToken.getType() == TokenType::identifier) {
-		m_name = theNameToken.getData();
+	if (m_tokenizer.more()) {
+		const Token& theNameToken = m_tokenizer.get();
+		if (theNameToken.getType() == TokenType::identifier) {
+			m_name = theNameToken.getData();
+		}
+		else {
+			theResult.setError(Error::identifier_expected, "Table name is not an identifier");
+		}
 	}
 	else {
-		theResult.setError(Error::identifier_expected, "Table name is not an identifier");
+		theResult.setError(Error::identifier_expected, "Table name missing");
 	}
 	return theResult;
 }
 
+/* -------------------------------Helper Functions------------------------------- */
+using TokenFilter = std::function<bool(const Token&)>;
+
+StatusResult parseSequence(Tokenizer& aTokenizer, StringList& aContainer, TokenFilter aFilter) {
+	StatusResult theResult(Error::no_error);
+	if (aTokenizer.more()) {
+		do {
+			const Token& theToken = aTokenizer.get();
+			if (aFilter(theToken)) {
+				aContainer.emplace_back(theToken.getData());
+			}
+			else {
+				theResult.setError(Error::syntax_error, "Invalid input '" + theToken.getData() + '\'');
+			}
+		} while (theResult && aTokenizer.skipIf(TokenType::comma));
+	}
+	else {
+		theResult.setError(Error::syntax_error, "Input not enough");
+	}
+	return theResult;
+}
+
+template<typename ElemT, typename CompT>
+std::string examineOverlap(const std::vector<ElemT>& aContainer, std::function<CompT(const ElemT&)> anExtracter) {
+	std::unordered_set<theCompElem> theCompElemSet;
+	for (const auto& theElem : aContainer) {
+		CompT theCompElem = anExtracter(theElem);
+		if (theCompElemSet.count(theCompElem)) {
+			return theCompElem;
+		}
+	}
+	return "";
+}
 
 /* -------------------------------CreateTableStatement------------------------------- */
 class CreateTableStatement : public SQLStatement {
@@ -205,14 +243,13 @@ private:
 	}
 
 	StatusResult _validateAttributes() const {
-		std::unordered_set<std::string> theAttrNames;
-		for (const auto& theAttribute : m_attributes) {
-			const std::string& theName = theAttribute.getName();
-			if (theAttrNames.count(theName)) {
-				return StatusResult(Error::invalid_attribute, '\'' + theName + "' redefinition");
+		std::string theOverlap = examineOverlap<Attribute, std::string>(m_attributes,
+			[](const Attribute& anAttribute)->std::string {
+				return anAttribute.getName();
 			}
-		}
-		return StatusResult(Error::no_error);
+		);
+		return theOverlap.empty() ?
+			StatusResult(Error::no_error) : StatusResult(Error::invalid_arguments, '\'' + theOverlap + "' redefined");
 	}
 
 	static bool _isValidAttributeOption(Keyword aKeyword) {
@@ -257,6 +294,109 @@ public:
 	}
 };
 
+/* -------------------------------InsertIntoTableStatement------------------------------- */
+class InsertIntoTableStatement : public SQLStatement {
+public:
+	InsertIntoTableStatement(Tokenizer& aTokenizer, SQLInterpreter& anInterpreter) :
+		SQLStatement(aTokenizer, anInterpreter)
+	{
+	}
+
+	~InsertIntoTableStatement() override = default;
+
+	StatusResult parse() override {
+		StatusResult theResult(Error::no_error);
+		if (m_tokenizer.next() && m_tokenizer.skipIf(Keyword::into_kw)) {
+			if (theResult = _parseTableName()) {
+				if (m_tokenizer.skipIf(TokenType::lparen)) {
+					if (theResult = _parseFieldNames()) {
+						if (m_tokenizer.skipIf(TokenType::rparen)) {
+							if (m_tokenizer.skipIf(Keyword::values_kw)) {
+								if (m_tokenizer.skipIf(TokenType::lparen)) {
+									if (theResult = _parseValueStrings()) {
+										if (m_tokenizer.skipIf(TokenType::rparen)) {
+											//  success
+										}
+										else {
+											theResult.setError(Error::syntax_error, "')' missing");
+										}
+									}
+								}
+								else {
+									theResult.setError(Error::syntax_error, "'(' missing after 'values'");
+								}
+							}
+							else {
+								theResult.setError(Error::syntax_error, "'values' missing");
+							}
+						}
+						else {
+							theResult.setError(Error::syntax_error, "')' missing");
+						}
+					}
+				}
+				else {
+					theResult.setError(Error::syntax_error, "'(' missing after table name '" + m_name + "'");
+				}
+			}
+		}
+		else {
+			theResult.setError(Error::keyword_expected, "'into'");
+		}
+		return theResult;
+	}
+
+	StatusResult validate() const override {
+		StatusResult theResult(Error::no_error);
+		if (!m_tokenizer.more()) {
+			if (m_fieldNames.size() != m_valueStrs.size()) {
+				theResult = _validateFieldNames();
+			}
+			else {
+				theResult.setError(Error::keyValue_mismatch, "The number of fields is not equal to the number of values");
+			}
+		}
+		else {
+			theResult.setError(Error::syntax_error, "Redundant input after ')'");
+		}
+		return theResult;
+	}
+
+	StatusResult execute() const override {
+
+	}
+private:
+	StringList m_fieldNames;
+	StringList m_valueStrs;
+
+	inline StatusResult _parseFieldNames() {
+		return parseSequence(m_tokenizer, m_fieldNames,
+			[](const auto& aToken)->bool {
+				return aToken.getType() == TokenType::identifier;
+			}
+		);
+	}
+
+	inline StatusResult _parseValueStrings() {
+		return parseSequence(m_tokenizer, m_valueStrs,
+			[](const auto& aToken)->bool {
+				TokenType theType = aToken.getType();
+				return theType == TokenType::string || theType == TokenType::number;
+			}
+		);
+	}
+
+	StatusResult _validateFieldNames() const {
+		std::string theOverlap = examineOverlap<std::string, std::string>(m_fieldNames,
+			[](const std::string& aFieldName)->std::string {
+				return aFieldName;
+			}
+		);
+		return theOverlap.empty() ?
+			StatusResult(Error::no_error) : StatusResult(Error::invalid_arguments, '\'' + theOverlap + "' redefined");
+	}
+};
+
 std::unique_ptr<SQLStatement> SQLStatement::factory(Tokenizer& aTokenizer, SQLInterpreter& anInterpreter) {
 	Keyword theKeyword = aTokenizer.peek().getKeyword();
 	switch (theKeyword) {
@@ -264,6 +404,8 @@ std::unique_ptr<SQLStatement> SQLStatement::factory(Tokenizer& aTokenizer, SQLIn
 		return std::make_unique<CreateTableStatement>(aTokenizer, anInterpreter);
 	case Keyword::describe_kw:
 		return std::make_unique<DescTableStatement>(aTokenizer, anInterpreter);
+	case Keyword::insert_kw:
+		return std::make_unique<InsertIntoTableStatement>(aTokenizer, anInterpreter);
 	default:
 		return nullptr;
 	}
