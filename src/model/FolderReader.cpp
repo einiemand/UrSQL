ce20e9a8d4720a881ee0fff106f4fb5933d3cb73
einiemand-sl <einiemand@outlook.com>
2019-07-30 18:53:58 -0500

    Add a directory entry iterator wrapper to deal with different system calls.

--- src/model/FolderReader.cpp
+++ src/model/FolderReader.cpp
@@ -8,15 +8,55 @@
 
 namespace UrSQL {
 
-bool directoryExists(const std::string& aDirPath) {
-	DWORD theFileAttributes = GetFileAttributesA(aDirPath.c_str());
-	return theFileAttributes != INVALID_FILE_ATTRIBUTES && (theFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
-}
+#ifdef _WIN32
+class DirEntIterator {
+public:
+	explicit DirEntIterator(const std::string& aDirPath) :
+		m_fileHandle(INVALID_HANDLE_VALUE),
+		m_fileData()
+	{
+		std::string theFileWildCard = aDirPath + "/*";
+		m_fileHandle = FindFirstFileA(theFileWildCard.c_str(), &m_fileData);
+	}
 
-bool isNotDir(const WIN32_FIND_DATAA& aFindData) {
-	return aFindData.dwFileAttributes != INVALID_FILE_ATTRIBUTES
-		&& !(aFindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
-}
+	~DirEntIterator() {
+		FindClose(m_fileHandle);
+	}
+
+	DirEntIterator(const DirEntIterator&) = delete;
+	DirEntIterator& operator=(const DirEntIterator&) = delete;
+
+	bool next() {
+		return FindNextFileA(m_fileHandle, &m_fileData);
+	}
+
+	bool isNotDir() const {
+		return (*this) && !(m_fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
+	}
+
+	explicit operator bool() const {
+		return m_fileHandle != INVALID_HANDLE_VALUE && m_fileData.dwFileAttributes != INVALID_FILE_ATTRIBUTES;
+	}
+
+	std::string currentFileName() const {
+		return std::string(m_fileData.cFileName);
+	}
+
+	static bool directoryExists(const std::string& aDirPath) {
+		DWORD theFileAttributes = GetFileAttributesA(aDirPath.c_str());
+		return theFileAttributes != INVALID_FILE_ATTRIBUTES && (theFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
+	}
+
+	static bool createDirectory(const std::string& aDirPath) {
+		return CreateDirectoryA(aDirPath.c_str(), nullptr);
+	}
+private:
+	HANDLE m_fileHandle;
+	WIN32_FIND_DATAA m_fileData;
+};
+#elif defined(__linux__) || defined(__APPLE__)
+
+#endif
 
 FolderReader::FolderReader(std::string aPath) :
 	m_path(std::move(aPath))
@@ -24,31 +64,25 @@ FolderReader::FolderReader(std::string aPath) :
 }
 
 StatusResult FolderReader::eachFile(FileVisitor aVisitor) const {
-#ifdef _WIN32
 	StatusResult theResult(Error::no_error);
-	if (!directoryExists(m_path)) {
-		if (!CreateDirectoryA(m_path.c_str(), nullptr)) {
-			theResult.setError(Error::folder_notExist, '\'' + m_path + '\'');
+	if (!DirEntIterator::directoryExists(m_path)
+		&& !DirEntIterator::createDirectory(m_path))
+	{
+		theResult.setError(Error::folder_notExist, '\'' + m_path + '\'');
+	}
+	if (theResult) {
+		DirEntIterator theDirEntIter(m_path);
+		if (theDirEntIter) {
+			do {
+				if (theDirEntIter.isNotDir()
+					&& !aVisitor(theDirEntIter.currentFileName()))
+				{
+					break;
+				}
+			} while (theDirEntIter.next());
 		}
 	}
-	WIN32_FIND_DATAA theFindData;
-	std::string theFileWildcard = m_path + "/*" + Storage::defaultFileExtension;
-	HANDLE theFileHandle = FindFirstFileA(theFileWildcard.c_str(), &theFindData);
-	if (theFileHandle != INVALID_HANDLE_VALUE) {
-		do {
-			if (isNotDir(theFindData)
-				&& !aVisitor(theFindData.cFileName))
-			{
-				break;
-			}
-		} while (FindNextFileA(theFileHandle, &theFindData));
-		FindClose(theFileHandle);
-	}
 	return theResult;
-#elif defined(__LINUX__) || defined(__APPLE__)
-
-#endif
 }
-
 
 }
