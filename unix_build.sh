49a26aa353de8d271518eec5ce5e3c64fbc59f0d
einiemand-aw-wsl2 <einiemand@outlook.com>
2020-11-10 17:13:43 -0800

    Update build script to build for clang and gcc. Update .gitignore accordingly.

--- unix_build.sh
+++ unix_build.sh
@@ -1,11 +1,25 @@
-if [ -d "build" ]
+build_for_compiler () {
+  local dir_name="build_$1"
+  if [ -d $dir_name ]
+  then
+    echo "Directory $dir_name exists, removing now"
+    rm -r $dir_name
+  fi
+
+  echo "Creating $dir_name directory"
+  mkdir $dir_name
+  cd $dir_name || { echo "Unable to create $dir_name folder, aborting..."; exit 1; }
+  cmake -DCMAKE_C_COMPILER=$1 -DCMAKE_CXX_COMPILER=$2 -DCMAKE_BUILD_TYPE=Debug ..
+  make -j8
+  cd ..
+}
+
+if [ -x $(which g++) ]
 then
-  echo "Folder build exists, removing now"
-  rm -r build
+build_for_compiler gcc g++
 fi
 
-echo "Creating build folder"
-mkdir build
-cd build || { echo "Unable to create build folder, aborting..."; exit 1; }
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j8
+if [ -x $(which clang++) ]
+then
+build_for_compiler clang clang++
+fi
