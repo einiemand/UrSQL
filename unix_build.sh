59bbcbdda64f633d169e8e3e30835c5af41fef65
einiemand-aw-wsl2 <einiemand@outlook.com>
2020-11-15 16:34:00 -0800

    Update unix build script to only build with one compiler(gcc or clang). Remove .vscode/ from repo.

--- unix_build.sh
+++ unix_build.sh
@@ -1,25 +1,41 @@
 build_for_compiler () {
-  local dir_name="build_$1"
-  if [ -d $dir_name ]
+  if [ $# -ne 2 ]
   then
-    echo "Directory $dir_name exists, removing now"
-    rm -r $dir_name
+    echo "The number of arguments passed to $FUNCNAME is not 2 (C compiler name and C++ compiler name)"
+    echo "The arguments were $@"
+    echo "exiting..."
+    exit 1
+  fi
+  
+  local proj_dir="$(dirname $(readlink -f $0))"
+  local build_dir="$proj_dir/build"
+  local bin_dir="$proj_dir/bin"
+
+  if [ -d $build_dir ]
+  then
+    echo "Directory $build_dir exists, removing now"
+    rm -r $build_dir
+  fi
+  if [ -d $bin_dir ]
+  then
+    echo "Directory $bin_dir exists, removing now"
+    rm -r $bin_dir
   fi
 
-  echo "Creating $dir_name directory"
-  mkdir $dir_name
-  cd $dir_name || { echo "Unable to create $dir_name folder, aborting..."; exit 1; }
-  cmake -DCMAKE_C_COMPILER=$1 -DCMAKE_CXX_COMPILER=$2 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$(pwd)/../bin_$1 ..
+  echo "Creating $build_dir directory"
+  mkdir $build_dir || { echo "Unable to create $build_dir, aborting..."; exit 1; }
+  cd $build_dir
+  cmake -DCMAKE_C_COMPILER=$1 -DCMAKE_CXX_COMPILER=$2 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$bin_dir ..
   make -j8
   cd ..
 }
 
-if [ -x $(which g++) ]
+if [ -x $(which clang) -a -x $(which clang++) ]
 then
-build_for_compiler gcc g++
-fi
-
-if [ -x $(which clang++) ]
+  build_for_compiler clang clang++
+elif [-x $(which gcc) -a -x $(which g++)]
 then
-build_for_compiler clang clang++
+  build_for_compiler gcc g++
+else
+  { echo "Unable to build: gcc and clang not detected"; exit 1; }
 fi
